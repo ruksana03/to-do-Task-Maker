0524ad7d606f6c27ec0892ff7eeb59ec69cb1b12
Ruksana <ruksana01official@gmail.com>
2023-12-22 19:48:52 +0600

    update google login

--- todo-task-maker/src/components/homeCmp/Banner.jsx
+++ todo-task-maker/src/components/homeCmp/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
             <div className="absolute inset-0 flex items-center justify-center">
                 <div className="text-center text-white z-10">
                     <h1 className="text-4xl font-bold mb-4">hello</h1>
-                    <Link to='dashboard'>
+                    <Link to='/dashboard'>
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ background: "linear-gradient(to left, #D88531 10%, #FDBB05 10%, #FF2F6B 10%, #67A044 50%)" }}>
                             Let’s Explore
                         </button>
--- todo-task-maker/src/pages/SocialLogin.jsx
+++ todo-task-maker/src/pages/SocialLogin.jsx
@@ -1,20 +1,48 @@
 import { FaGoogle } from "react-icons/fa";
-
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-hot-toast'
+import useAuth from "../Hooks/useAuth";
+import {storeUserInfo } from "../API/verify";
 
 const SocialLogin = () => {
-    return (
-        <>
-        <button
-        //   onClick={handleGoogleSignIn}
-          className="w-full mt-8 flex justify-center gap-6 items-center border-[1px] px-6 py-2 rounded-full shadow-md shadow-white hover:shadow-2xl hover:text-lg "
-          
-        >
-          <FaGoogle />
-          {/* <img src={google} alt="" width={30} height={30} /> */}
-          <p className="">Sign Up With Google</p>
-        </button>
-      </>
-    );
+  const navigate = useNavigate();
+  const { signInWithGoogle } = useAuth();
+  const from = location?.state?.from?.pathname || '/'
+
+  // Handle Google Signin function
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithGoogle()
+
+      // send user info to database 
+      const sendToDb = await storeUserInfo(result?.user)
+      console.log(sendToDb)
+
+
+
+      // navigate to path
+      navigate(from, { replace: true })
+
+      // show toast
+      toast.success("register Successfully")
+
+    } catch (err) {
+      toast.error(err?.message)
+    }
+  }
+  return (
+    <>
+      <button
+        onClick={handleGoogleSignIn}
+        className="w-full mt-8 flex justify-center gap-6 items-center border-[1px] px-6 py-2 shadow-md shadow-white hover:shadow-2xl hover:text-lg "
+     
+      >
+        <FaGoogle />
+        {/* <img src={google} alt="" width={30} height={30} /> */}
+        <p className="">Sign Up With Google</p>
+      </button>
+    </>
+  );
 };
 
 export default SocialLogin;
